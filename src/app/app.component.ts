158517f30da622a596a8ef4bff58459518ecff3f
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-11-12 17:39:25 -0300

    login, header and footer components added (no ts yet)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'employee-control-system';
