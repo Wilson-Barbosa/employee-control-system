8b081bd6dbe95ae9b861019db1ce7a00b357d6cd
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-11-15 18:32:21 -0300

    register, search and side-menu components added (no TS yet)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { LoginComponent } from "./components/login/login.component";
+import { SearchComponent } from "./components/search/search.component";
+import { SideMenuComponent } from "./components/side-menu/side-menu.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, LoginComponent]
+    imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, LoginComponent, SearchComponent, SideMenuComponent, RegisterComponent]
 })
 export class AppComponent {
   title = 'employee-control-system';
